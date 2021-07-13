d398f3263f691ebb69231fa0eec4cb6d299ceaf6
Philippe Ombredanne <pombredanne@nexb.com>
2021-07-13 08:20:26 +0200

    Mark Python 3.9 as supported
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ def main():
                        'Programming Language :: Python',
                        'Programming Language :: Python :: 3.7',
                        'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
                        'Topic :: Software Development :: Libraries',
                        'Topic :: Utilities'],
           entry_points={
