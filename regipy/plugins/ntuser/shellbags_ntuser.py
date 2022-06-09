c0f6ed4d3ead15f12bb064d386006effd082a827
tincho9 <martin.k@sygnia.co>
2022-06-09 11:37:02 +0300

    Fix dates

--- regipy/plugins/ntuser/shellbags_ntuser.py
+++ regipy/plugins/ntuser/shellbags_ntuser.py
@@ -12,23 +12,26 @@ NTUSER_SHELLBAG = '\\Software\\Microsoft\\Windows\\Shell\\BagMRU'
 USRCLASS_SHELLBAG = '\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\BagMRU'
 CODEPAGE = 'cp1252'
 
+
 class ShellBagNtuserPlugin(Plugin):
     NAME = 'shellbag_plugin'
     DESCRIPTION = 'Parse Shellbag items'
     COMPATIBLE_HIVE = NTUSER_HIVE_TYPE
 
-    def _parse_MRU(self,mru_val):
+    @staticmethod
+    def _parse_mru(mru_val):
         mru_order_string = ''
-        if isinstance(mru_val,bytes):
+        if isinstance(mru_val, bytes):
             mru_val = mru_val[:-4]
-            for i in range(0,len(mru_val),4):
+            for i in range(0, len(mru_val), 4):
                 mru_order_string += f'{str(mru_val[i])}-'
 
             return mru_order_string[:-1]
         else:
             return mru_order_string
 
-    def _get_shell_item_type(self,shell_item):
+    @staticmethod
+    def _get_shell_item_type(shell_item):
 
         if isinstance(shell_item, pyfwsi.volume):
             item_type = "Volume"
@@ -44,7 +47,8 @@ class ShellBagNtuserPlugin(Plugin):
 
         return item_type
 
-    def _parse_shell_Item_path_segment(self,shell_item):
+    @staticmethod
+    def _parse_shell_item_path_segment(shell_item):
         """Parses a shell item path segment.
         Args:
           shell_item (pyfwsi.item): shell item.
@@ -79,13 +83,12 @@ class ShellBagNtuserPlugin(Plugin):
 
         return path_segment
 
-    def iter_sk(self,key, reg_path, path='root'):
+    def iter_sk(self, key, reg_path, path='root'):
 
         last_write = convert_wintime(key.header.last_modified, as_json=True)
 
-
         mru_val = key.get_value('MRUListEx')
-        mru_order = self._parse_MRU(mru_val)
+        mru_order = self._parse_mru(mru_val)
 
         if key.get_value('NodeSlot'):
             node_slot = str(key.get_value('NodeSlot'))
@@ -100,11 +103,11 @@ class ShellBagNtuserPlugin(Plugin):
                 shell_items.copy_from_byte_stream(byte_stream, ascii_codepage=CODEPAGE)
                 for item in shell_items.items:
                     shell_type = self._get_shell_item_type(item)
-                    value = self._parse_shell_Item_path_segment(item)
+                    value = self._parse_shell_item_path_segment(item)
                     path = f'{path}\\{value}'
 
-                    creation_time = ''
-                    access_time = ''
+                    creation_time = None
+                    access_time = None
 
                     if len(item.extension_blocks) > 0:
                         for extension_block in item.extension_blocks:
@@ -112,7 +115,7 @@ class ShellBagNtuserPlugin(Plugin):
                                 creation_time = extension_block.get_creation_time().isoformat()
                                 access_time = extension_block.get_access_time().isoformat()
 
-                    if hasattr(item,'modification_time'):
+                    if hasattr(item, 'modification_time'):
                         modification_time = item.get_modification_time().isoformat()
                     else:
                         modification_time = None
@@ -128,34 +131,16 @@ class ShellBagNtuserPlugin(Plugin):
                              'access_time': access_time,
                              'modification_time': modification_time,
                              'last_write': last_write,
-                             'mru_order' : mru_order}
+                             'mru_order': mru_order}
 
                     self.entries.append(entry)
 
                     sk = self.registry_hive.get_key(sk_reg_path)
                     self.iter_sk(sk, sk_reg_path, path)
 
-
-
-
-
-
     def run(self):
         try:
             shellbag_ntuser_subkey = self.registry_hive.get_key(NTUSER_SHELLBAG)
             self.iter_sk(shellbag_ntuser_subkey, NTUSER_SHELLBAG)
-
-
-
         except RegistryKeyNotFoundException as ex:
             logger.error(f'Could not find {self.NAME} plugin data at: {NTUSER_SHELLBAG}: {ex}')
-
-
-
-
-
-
-
-
-
-
