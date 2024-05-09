3cf3584d0b290e4cbf9fed231f7df9922b1162d3
lotansery7 <lotan.s@sygnia.co>
2024-05-09 16:37:25 +0300

    add bam path

--- regipy/plugins/system/bam.py
+++ regipy/plugins/system/bam.py
@@ -9,7 +9,8 @@ from regipy.utils import convert_wintime
 
 logger = logging.getLogger(__name__)
 
-BAM_PATH = r'Services\bam\UserSettings'
+BAM_PATH = [r'Services\bam\UserSettings',
+            r'Services\bam\state\UserSettings']
 
 
 class BAMPlugin(Plugin):
@@ -20,35 +21,37 @@ class BAMPlugin(Plugin):
     def run(self):
         logger.debug('Started Computer Name Plugin...')
 
-        try:
-            for subkey_path in self.registry_hive.get_control_sets(BAM_PATH):
-                subkey = self.registry_hive.get_key(subkey_path)
-                for sid_subkey in subkey.iter_subkeys():
+        for key_path in BAM_PATH:
+            try:
+                for subkey_path in self.registry_hive.get_control_sets(key_path):
+                    subkey = self.registry_hive.get_key(subkey_path)
+                    for sid_subkey in subkey.iter_subkeys():
 
-                    sid = sid_subkey.name
-                    logger.debug(f'Parsing BAM for {sid}')
-                    sequence_number = None
-                    version = None
-                    entries = []
+                        sid = sid_subkey.name
+                        logger.debug(f'Parsing BAM for {sid}')
+                        sequence_number = None
+                        version = None
+                        entries = []
 
-                    for value in sid_subkey.get_values(trim_values=self.trim_values):
-                        if value.name == 'SequenceNumber':
-                            sequence_number = value.value
-                        elif value.name == 'Version':
-                            version = value.value
-                        else:
-                            entries.append({
-                                'executable': value.name,
-                                'timestamp': convert_wintime(Int64ul.parse(value.value), as_json=self.as_json)
-                            })
+                        for value in sid_subkey.get_values(trim_values=self.trim_values):
+                            if value.name == 'SequenceNumber':
+                                sequence_number = value.value
+                            elif value.name == 'Version':
+                                version = value.value
+                            else:
+                                entries.append({
+                                    'executable': value.name,
+                                    'timestamp': convert_wintime(Int64ul.parse(value.value), as_json=self.as_json)
+                                })
 
-                    self.entries.extend([
-                        {
-                            'sequence_number': sequence_number,
-                            'version': version,
-                            'sid': sid,
-                            **x
-                        } for x in entries]
-                    )
-        except RegistryKeyNotFoundException as ex:
-            logger.error(ex)
+                        self.entries.extend([
+                            {
+                                'sequence_number': sequence_number,
+                                'version': version,
+                                'sid': sid,
+                                'key_path': f'{subkey_path}\\{sid}',
+                                **x
+                            } for x in entries]
+                        )
+            except RegistryKeyNotFoundException as ex:
+                logger.error(ex)
