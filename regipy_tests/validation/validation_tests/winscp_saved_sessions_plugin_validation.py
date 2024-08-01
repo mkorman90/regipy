8bd742c412e8f57d1027f6d5cd9068a3ca606718
Martin SK <martin@fedora.tail09afc.ts.net>
2024-08-01 18:47:37 +0300

    nit

--- regipy_tests/validation/validation_tests/winscp_saved_sessions_plugin_validation.py
+++ regipy_tests/validation/validation_tests/winscp_saved_sessions_plugin_validation.py
@@ -6,7 +6,3 @@ class WinSCPSavedSessionsPluginValidationCase(ValidationCase):
     plugin = WinSCPSavedSessionsPlugin
     test_hive_file_name = "ntuser_hive_2.xz"
     expected_entries_count = 2
-
-    # TODO: Replace hive test files, as some seem a bit sensitive and some might infrige licenses... 
-    # @nocommit
-    
